2d4b41e54468500a544b235adb14b7dfe9125fe3
Pedro Henrique <71939458+Tdk17@users.noreply.github.com>
2026-09-16 13:20:19 -0300

    refactor: remove stale routes from legacy interface

--- lib/Src/Router/rotas.dart
+++ lib/Src/Router/rotas.dart
@@ -1,18 +1,11 @@
-import 'package:app_academia/Src/Home/home_screen.dart';
-import 'package:app_academia/Src/Treinos/treinoPage.dart';
-import 'package:app_academia/Src/componets/bottom_navBar.dart';
-import 'package:app_academia/Src/dashboard/perfil_board.dart';
 import 'package:go_router/go_router.dart';
+import '../componets/bottom_navBar.dart';
 
 final appRouter = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const MainNavigation()),
-    GoRoute(path: 'treinos', builder: (context, state) => const TreinoPage()),
     GoRoute(
-      path: '/perfil',
-      builder: (context, state) => const DashboardPage(),
+      path: '/',
+      builder: (context, state) => const MainNavigation(),
     ),
-    GoRoute(path: '/detail', builder: (context, state) => TreinoDetalhePage()),
-    GoRoute(path: '/back', builder: (context, state) => const DashboardPage()),
   ],
 );
